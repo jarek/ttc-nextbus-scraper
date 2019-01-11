7c9df955e8386e88af1d0410983ad9d3f5c04b30
Jarek Piórkowski <jarek@piorkowski.ca>
2019-01-11 05:07:40 -0800

    incantation for mobile browsers sensible rendering

--- index.py
+++ index.py
@@ -72,6 +72,7 @@ def print_header():
     print('''<!doctype html>
 
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width">
 <title>TTC</title>
 
 <style type="text/css">
