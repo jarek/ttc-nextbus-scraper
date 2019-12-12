1e9a4c188aba7e8c457d2d6191e5ac0596e5a78f
Jarek Piórkowski <jarek@piorkowski.ca>
2019-12-11 20:06:39 -0800

    nicer python style

--- index.py
+++ index.py
@@ -11,33 +11,32 @@ DESTINATIONS = {
     'West - 504b King towards Dufferin Gate': 'Dufferin',
     'West - 504a King towards Dundas West Station': 'Dundas West',
     'West - 304 King towards Dundas West Station': 'Dundas West',
+    'West - 504s King Short Turn towards Roncesvalles and Queen': 'Sunnyside',
     'East - 504b King towards Broadview Station': 'Broadview',
-    'East - 504a King towards Distillery': 'Distillery'
+    'East - 504a King towards Distillery': 'Distillery',
+    'East - 504s King Short Turn towards Broadview Ave': 'Broadview and Queen',
 }
 
 def parse_url(address):
-    # grab the xml
     text = urllib2.urlopen(address).read()
     root = ET.fromstring(text)
+    all_predictions = root[0]
 
-    directions = root[0]
+    def _destination_name(elem):
+        dest_text = elem.attrib['title']
+        # check if there is a better text, fall back to provided if not
+        return DESTINATIONS.get(dest_text, dest_text)
 
-    results = {}
+    def _predictions(elem):
+        return [int(prediction.attrib['minutes']) for prediction in elem]
 
-    for destination in directions:
-        if destination.tag != 'direction': continue
-
-        dest_text = destination.attrib['title']
-        # fall check if there is a better text, back to provided if not
-        dest_name = DESTINATIONS.get(dest_text, dest_text)
-
-        results[dest_name] = [
-                int(prediction.attrib['minutes'])
-                for prediction in destination]
+    results = {_destination_name(destination): _predictions(destination)
+               for destination in all_predictions
+               if destination.tag == 'direction'}
 
     return results
 
-def print_stop(name, route, stop, destinations=None):
+def format_stop(name, route, stop, destinations=None):
     result = ''
 
     html_url = HTML_URL.format(route=route, stop=stop)
@@ -50,17 +49,16 @@ def print_stop(name, route, stop, destinations=None):
     try:
         data = parse_url(xml_url)
 
-        if not destinations:
-            predictions_for_destinations = sorted(
-                    time
-                    for times in data.values()
-                    for time in times)
-        else:
-            predictions_for_destinations = sorted(
-                    time
-                    for destination, times in data.items()
-                    for time in times
-                    if destination in destinations)
+        if destinations:
+            # filter to only requested destinations
+            data = {destination: times for destination, times in data.items()
+                    if destination in destinations}
+
+        # collapse all destinations together and
+        # sort to earliest times regardless of destination
+        predictions_for_destinations = sorted(time
+                                              for times in data.values()
+                                              for time in times)
 
         result += ', '.join(str(p) for p in predictions_for_destinations)
         result += ' minutes'
@@ -68,7 +66,7 @@ def print_stop(name, route, stop, destinations=None):
     except:
         result += ' unable to fetch'
 
-    print(result)
+    return result
 
 def print_header():
     print('''<!doctype html>
@@ -93,19 +91,19 @@ toronto_now = server_now + datetime.timedelta(hours=3)
 print('as of ' + toronto_now.strftime('%H:%M:%S'))
 
 print('<h2>504 @ Shaw</h2>')
-print_stop('Eastbound', 504, 5422)
-print_stop('Westbound to Dundas West', 504, 8560, ['Dundas West'])
+print(format_stop('Eastbound', 504, 5422))
+print(format_stop('Westbound to Dundas West', 504, 8560, ['Dundas West']))
 
 print('<h2>63 @ Sudbury</h2>')
-print_stop('Northbound', 63, 8998)
+print(format_stop('Northbound', 63, 8998))
 
 print('\n<hr>\n')
 
 print('<h2>504 @ Yonge / King Stn</h2>')
-print_stop('Westbound', 504, 23884)  # this is TTC stop 15637, not sure why NextBus is different
+print(format_stop('Westbound', 504, 23884))  # this is TTC stop 15637, not sure why NextBus is different
 
 print('\n<hr>\n')
 
 print('<h2>63 @ Ossington Stn</h2>')
-print_stop('Southbound', 63, 14265)
+print(format_stop('Southbound', 63, 14265))
 
